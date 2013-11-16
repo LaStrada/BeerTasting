df28ca058a6a39c139fca424cd7ef8471e050aea
LaStrada <stale.hauknes@gmail.com>
2013-11-16 02:06:10 +0100

    Finishing rating system
    Fixes #10 and #7

--- BeerTasting/templatetags/beertasting_extra.py
+++ BeerTasting/templatetags/beertasting_extra.py
@@ -4,9 +4,15 @@ from Beers.models import Setup
 register = template.Library()
 
 @register.filter(name="print_stars")
-def print_stars(rating):
+def print_stars(ratings, b_id):
+    rating = 0
+    comment = ''
+    
     try:
-        int(rating)
+        for r in ratings:
+            if b_id == r.beer_id:
+                rating = int(r.rating)
+                comment = r.comment
             
     except (ValueError, TypeError):
         rating = 0
@@ -20,11 +26,13 @@ def print_stars(rating):
         else:
             string += '<span class="glyphicon glyphicon-star-empty"></span>'
     
+    if comment:
+        string += "<br />%s" % comment
+    
     return string
 
 @register.filter(name="print_stars_form")
 def print_stars_form(rating):
-#     return "lol"
     try:
         int(rating)
             
@@ -35,10 +43,10 @@ def print_stars_form(rating):
     
     for x in range(0,10):
         if (x+1) == rating:
-            string += '<input type="radio" name="star" id="%d" value="%d"checked />&nbsp;' % (x, x)
+            string += '<input type="radio" name="star" id="%d" value="%d"checked />&nbsp;' % (x, x+1)
             string += '<label for="%d"><span class="glyphicon glyphicon-star"></span></label>' % x
         else:
-            string += '<input type="radio" name="star" id="%d" value="%d" />&nbsp;' % (x, x)
+            string += '<input type="radio" name="star" id="%d" value="%d" />&nbsp;' % (x, x+1)
             string += '<label for="%d"><span class="glyphicon glyphicon-star-empty"></span></label>' % x
     
     string += '</div>'
--- Beers/views.py
+++ Beers/views.py
@@ -12,26 +12,15 @@ from django.template import RequestContext, loader
 from django.db.models import Count
 
 def index(request):
-    #beers = Beer.objects.all()
-    beers = Beer.objects.raw('SELECT * FROM Beers_beer LEFT JOIN Beers_beerrating ON Beers_beer.id=Beers_beerrating.beer_id')
+    beers = Beer.objects.all()
+#    beers = BeerRating.objects.raw('''SELECT *
+#                                FROM Beers_beer
+#                                LEFT JOIN Beers_beerrating
+#                                ON Beers_beer.id=Beers_beerrating.beer_id''')
     
-    return render(request, 'index.html', {'beers':beers, 'login_failed':False, 'Finished':True})
-
-
-# def selected_beer(request):
-#     #beer = Beer.objects.all()
-#     #ratings = BeerRating.objects.all().filter(user=1)
-#     #beer = Beer.objects.all().prefetch_related('id__id')
-#     #beers = Beer.objects.select_related('rating')
-#     #beers = Beer.objects.all().prefetch_related('ratings') 
-#     
-# #     beers = Beer.objects.raw('SELECT * FROM Beers_beer LEFT JOIN Beers_beerrating ON Beers_beer.id=Beers_beerrating.beer_id')
-#     
-#     cursor = connection.cursor()
-#     cursor.execute("SELECT * FROM Beers_beer LEFT JOIN Beers_beerrating ON Beers_beer.id=Beers_beerrating.beer_id")
-#     a = cursor.fetchall()
-#     
-#     return render(request, 'selected_beer.html', {'beers':beers})
+    ratings = BeerRating.objects.filter(user_id=request.user.id)
+    
+    return render(request, 'index.html', {'beers':beers, 'ratings':ratings, 'login_failed':False, 'Finished':False})
 
 
 def stats(request):
@@ -57,27 +46,36 @@ def rate_beer(request, beer_id):
     if request.method == 'POST':
         #Validate
         #todo: validate message and rating
-        
-        #Update
-        if beers.count() == 1:
-            beer = BeerRating.objects.get(user=request.user.id, beer=b_id)
-            
-            beer.comment = request.POST['comment']
-            beer.rating = int(request.POST['star'])
-            beer.save()
-            return HttpResponseRedirect(reverse('index'))
-            
-        #Insert
-        elif beers.count == 0:
-            new_rating = BeerRating(user=request.user.id, beer=b_id, rating=request.POST['star'], comment=request.POST['comment'])
-            new_rating.save()
-            return HttpResponseRedirect(reverse('index'))
-            
-        #Too many posts
+        if int(request.POST['star']):
+            #Update
+            if beers.count() == 1:
+                beer = BeerRating.objects.get(user=request.user.id, beer=b_id)
+                
+                beer.comment = request.POST['comment']
+                beer.rating = int(request.POST['star'])
+                beer.save()
+                return HttpResponseRedirect(reverse('index'))
+                
+            #Insert
+            elif beers.count() == 0:
+                new_rating = BeerRating(user=request.user, beer_id=b_id, rating=request.POST['star'], comment=request.POST['comment'])
+                new_rating.save()
+                return HttpResponseRedirect(reverse('index'))
+                
+            #Too many posts
+            else:
+                errors = "Too many posts..."
+                
         else:
-            errors = "Too many posts..."
-            
-    return render(request, 'rate_beer.html', {'beers':beers, 'b_id':b_id, 'errors':errors})
+            errors = "Not a valid rating value..."
+    
+    
+    try:
+        beer = BeerRating.objects.get(user=request.user.id, beer=b_id)
+    except:
+        raise Http404
+    
+    return render(request, 'rate_beer.html', {'beer':beer, 'b_id':b_id, 'errors':errors})
 
 
 def login_view(request):
