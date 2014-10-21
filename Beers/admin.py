bcdc9f6f87547f3c748e722dc8ce68c923dc0eca
Ståle Semb Hauknes <lolBookAir>
2014-10-21 13:46:12 +0200

    Delete model

--- Beers/admin.py
+++ Beers/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from Beers.models import Setup, Brewery, BeerStyle, Beer, BeerRating, UntappdUser
+from Beers.models import Setup, BeerStyle, Beer, BeerRating, UntappdUser
 
 admin.autodiscover()
 
 admin.site.register(Setup)
-admin.site.register(Brewery)
 admin.site.register(BeerStyle)
 admin.site.register(Beer)
 admin.site.register(BeerRating)
